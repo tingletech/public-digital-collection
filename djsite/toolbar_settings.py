a84bc208c81ee1348b88e2e8b124de0195be6ee8
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2011-07-09 13:31:39 -0700

    use ../bin/python manage.py runserver --settings=toolbar_settings to get the django debug toolbar

---
+++ djsite/toolbar_settings.py
@@ -0,0 +1,24 @@
+from settings import *
+
+DEBUG_TOOLBAR = True
+ 
+# Always show the toolbar 
+DEBUG_TOOLBAR_CONFIG = { 
+    'SHOW_TOOLBAR_CALLBACK': lambda req: True, 
+} 
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'debug_toolbar',
+)
+
--- virtualenv.bash
+++ virtualenv.bash
@@ -1,6 +1,7 @@
 set -eu
 which virtualenv
-cd $PREFIX
+#cd $PREFIX
 virtualenv  .
 ./bin/easy_install django
 ./bin/easy_install solrpy
+./bin/easy_install django-debug-toolbar
