11790a7bc074d331a45de4227f340370e0ed087b
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2011-08-16 01:02:04 -0700

    need more memory to reindex

--- ENV.solr-reindex.bash
+++ ENV.solr-reindex.bash
@@ -1,7 +1,7 @@
 export PREFIX=$HOME/public-digital-collection
 export CATALINA_BASE=$PREFIX/java-homes/catalina_reindex
 export CATALINA_HOME=$PREFIX/tomcat
-export CATALINA_OPTS="-server"
+export CATALINA_OPTS="-server -Xmx2g"
 export JAVA_HOME=/cdlcommon/products/jdk1.6.0_23
 export PATH="$JAVA_HOME/bin:/cdlcommon/products/bin:$PATH"
 export JAVA_OPTS=" -Dsolr.solr.home=$PREFIX/java-homes/solr_reindex -Dsolr.data.dir=$PREFIX/java-homes/solr_reindex/data -Dsolr.clustering.enabled=true"
